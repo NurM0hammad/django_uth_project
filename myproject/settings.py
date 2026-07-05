25ed6ab1533698c82ec15aecca9f8bd3b071fa1d
Nur Mohammad <nur.mohammad.cyber@gmail.com>
2026-07-05 19:48:09 +0600

    login html error

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x-hns^&pt!j97h6**%4hl1))*$#=_6*=w^s(h1ciiw=^fb-#5@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["django-uth-project.onrender.com"]
 
